38936cfb82f2985f53a176102ef981406c117a71
Nate Marks <npmarks@gmail.com>
2026-03-10 08:44:10 -0400

    golang update

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-GO_VERSION="1.25.1"
+GO_VERSION="1.25.8"
 
 mkdir -p ~/bin/go/"${GO_VERSION}"
 curl -L  "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz" \
