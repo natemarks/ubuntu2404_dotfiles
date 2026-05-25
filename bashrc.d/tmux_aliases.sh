318ad30fb83ae0af2b84a036bad0b6d7aa306905
Nate Marks <npmarks@gmail.com>
2026-05-25 09:40:45 -0400

    simplify

--- bashrc.d/tmux_aliases.sh
+++ bashrc.d/tmux_aliases.sh
@@ -13,13 +13,20 @@ alias tn='tmux new-session'
 # Lists all ongoing sessions
 alias tl='tmux list-sessions'
 
-# Interactive session selector using sesh and fzf
+# Interactive session selector using sesh and fzf with project directory picker
 function t() {
   {
     exec </dev/tty
     exec <&1
     local session
-    session=$(sesh list -t -c -z -i | fzf --ansi --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+    session=$(sesh list | fzf --ansi --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ' \
+      --header '  ^a all ^t tmux ^g configs ^x zoxide ^f projects' \
+      --bind 'tab:down,btab:up' \
+      --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list)' \
+      --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t)' \
+      --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c)' \
+      --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z)' \
+      --bind 'ctrl-f:change-prompt(🚀  )+reload(find ~/projects -mindepth 1 -maxdepth 1 -type d)')
     [[ -z "$session" ]] && return
 
     # Use --switch flag if we're inside tmux, otherwise attach normally
