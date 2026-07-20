fbc59dfc92c33c4126dbb33d94938b438612143a
Nate Marks <npmarks@gmail.com>
2026-07-20 08:12:33 -0400

    updat ego

--- bashrc.d/go.sh
+++ bashrc.d/go.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-export PATH=$PATH:~/bin/go/1.25.8/go/bin:~/go/bin
+export PATH=$PATH:~/bin/go/1.26.5/go/bin:~/go/bin
--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-GO_VERSION="1.25.8"
+GO_VERSION="1.26.5"
 
 mkdir -p ~/bin/go/"${GO_VERSION}"
 curl -L  "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz" \
